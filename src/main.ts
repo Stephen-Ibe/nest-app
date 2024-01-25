f5b451e6d3bf389080ae18e3cc7d9127d74fa488
Stephen Ibe <saintkalson@gmail.com>
2024-01-25 05:41:58 +0100

    global guard& public decorator

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AtGuard } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new AtGuard());
 
   await app.listen(3333);
 }
